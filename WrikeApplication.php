dd3263037f22875f61350e1f2286981ec14220c0
abdullahvolgsol <abdullah.ameen@volgsol.com>
2021-10-14 18:56:38 +0500

    Updated $ modifed variables and comments implementation of wrike Application

--- WrikeApplication.php
+++ WrikeApplication.php
@@ -2,119 +2,118 @@
 header("Access-Control-Allow-Origin: *");
 include('inc/header.php');
 include('src/wrike.php');
-$wrike = new wrike();	   		
+$wrike = new wrike();            
 
 ?>
 <?php include('inc/container.php');?>
 <div class="container">
-	<h2>Simple REST API with PHP and MySQL and wrike</h2>	
-	<br>
-	<br>
-	<form action="" method="POST">
-		<div class="form-group">
-			<h3>Client id</h3>
-			<br>			
-			<input type="text" name="client_id" value="luaXb1gH" class="form-control" required/>
-			<h3>Client secret</h3>
-			<br>			
-			<input type="text" name="client_secret" value="3K9WM4aANzzYNuYMNkPJulMXrFieCIoZRrbbaY7W2N6NOr6xyhC0tLKXYhh5j7S9" class="form-control" required/>
-		</div>
-		<button type="submit" name="userdetail" class="btn btn-default">SUBMIT</button>
-	</form>
-	<p>&nbsp;</p>
-	<?php
-	if(isset($_POST['userdetail']))	{
-		$client_id = $_POST['client_id'];
-		$client_secret = $_POST['client_secret'];
-   		$getInitaillize=$wrike->Initailization($client_id,$client_secret);
+   <h2>Simple REST API with PHP and MySQL for Wrike Application</h2> 
+   <br>
+   <br>
+   <form action="" method="POST">
+      <div class="form-group">
+         <h3>Client id</h3>
+         <br>        
+         <input type="text" name="client_id" value="Enter Client Id" class="form-control" required/>
+         <h3>Client secret</h3>
+         <br>        
+         <input type="text" name="client_secret" value="Enter Client Secret" class="form-control" required/>
+      </div>
+      <button type="submit" name="userdetail" class="btn btn-default">SUBMIT</button>
+   </form>
+   <p>&nbsp;</p>
+   <?php
+   if(isset($_POST['userdetail']))  {
+      $client_id = $_POST['client_id'];
+      $client_secret = $_POST['client_secret'];
+         $getInitaillize=$wrike->Initailization($client_id,$client_secret);
 
-   		if ($getInitaillize==-1) {
-   			?>
-   			<script type="text/javascript">
-   			alert("Incorrect client_id");
-   			</script>
-   			<?php 
-   		}
+         if ($getInitaillize==-1) {
+            ?>
+            <script type="text/javascript">
+            alert("Incorrect client_id");
+            </script>
+            <?php 
+         }
 
-   	
+      
 }
-	?>	
-		<form action="" method="POST">
-		<div class="form-group">
-			<h3>Authorize code</h3>
-		
-			<br>			
-			<input type="text" name="code" value="Enter code" class="form-control" required/>
-		</div>
-		<button type="submit" name="codesubmit" class="btn btn-default">SUBMIT</button>
-	</form>
-	<p>&nbsp;</p>
-	<?php
-	if(isset($_POST['codesubmit']))	{
-		$code = $_POST['code'];
-		//$code="A8ekmHwSsyoMgeYTsxaWmOfFrHNz9hQuP85jKsPZd6MLotVlHScfuIsHWXy0WOUZ_eu-N";
-		$getAccessToken=$wrike->generateAccessToken($code);
-   		if ($getAccessToken==-1) {
-   			?>
-   			<script type="text/javascript">
-   			alert("Not Found AccessToken");
-   			</script>
-   			<?php 
-   		}
- 		else
- 		{
- 			?>
-   			<script type="text/javascript">
-   			alert("Finally Found AccessToken");
-   			</script>
-   			<?php 
+   ?> 
+      <form action="" method="POST">
+      <div class="form-group">
+         <h3>Authorize code</h3>
+      
+         <br>        
+         <input type="text" name="code" value="Enter Code" class="form-control" required/>
+      </div>
+      <button type="submit" name="codesubmit" class="btn btn-default">SUBMIT</button>
+   </form>
+   <p>&nbsp;</p>
+   <?php
+   if(isset($_POST['codesubmit']))  {
+      $code = $_POST['code'];
+      $getAccessToken=$wrike->generateAccessToken($code);
+         if ($getAccessToken==-1) {
+            ?>
+            <script type="text/javascript">
+            alert("Not Found AccessToken");
+            </script>
+            <?php 
+         }
+      else
+      {
+         ?>
+            <script type="text/javascript">
+            alert("Finally Found AccessToken");
+            </script>
+            <?php 
 
- 		}
-   		
+      }
+         
 }
 ?>
-		<form action="" method="POST">
-		<div class="form-group">
-			<h3>Test Connection</h3>
-			<br>
-			<button type="submit" name="testconnection" class="btn btn-default">Test Connectiion</button>
-	</form>
-	<p>&nbsp;</p>
-	<?php
-	if(isset($_POST['testconnection']))	{
- 		$Isfindcontacts=$wrike->Usingaccesstokenfindcontacts();
- 			if ($Isfindcontacts==-1) {
-   			?>
-   			<script type="text/javascript">
-   			alert("Not Found Contacts");
-   			</script>
-   			<?php 
-   			}
-   			if ($Isfindcontacts[0]=='Access token is unknown or invalid') {
-   				$wrike->generaterefreshAccessToken();
-   				if ($Isfindcontacts==-1) {
-   					?>
-   						<script type="text/javascript">
-   						alert("Not Found Token");
-   						</script>
-   					<?php 
-   				}
-   				else
-   				{
-   					?>
-   						<script type="text/javascript">
-   						alert("Token is Refreshed");
-   						</script>
-   					<?php 
-   				}	
-   			}
-   			else
-   			{
-   				print_r($Isfindcontacts);
+      <form action="" method="POST">
+      <div class="form-group">
+         <h3>Test Connection</h3>
+         <br>
+         <button type="submit" name="testconnection" class="btn btn-default">Test Connectiion</button>
+   </form>
+   <p>&nbsp;</p>
+   <?php
+   if(isset($_POST['testconnection'])) {
+      $IsFindContacts=$wrike->Usingaccesstokenfindcontacts();
+         if ($IsFindContacts==-1) {
+            ?>
+            <script type="text/javascript">
+            alert("Not Found Contacts");
+            </script>
+            <?php 
+            }
+            if ($IsFindContacts[0]=='Access token is unknown or invalid') {
+               $wrike->generaterefreshAccessToken();
+               if ($IsFindContacts==-1) {
+                  ?>
+                     <script type="text/javascript">
+                     alert("Not Found Token");
+                     </script>
+                  <?php 
+               }
+               else
+               {
+                  ?>
+                     <script type="text/javascript">
+                     alert("Token is Refreshed");
+                     </script>
+                  <?php 
+               }  
+            }
+            else
+            {
+               print_r($IsFindContacts);
 
 
-   			}	
- 	
+            }  
+   
 }
 ?>
 
--- src/wrike.php
+++ src/wrike.php
@@ -6,58 +6,49 @@ class Wrike
     public $database = false;
     public function __construct()
     {
-    $servername  = 'localhost';
-    $username  = 'root';
+    $serverName  = 'localhost';
+    $userName  = 'root';
     $password   = "";
-    $databasename  = "wrikes";      
-    $this->database = mysqli_connect($servername, $username, $password,$databasename);  
-    
+    $databaseName  = "wrikes";      
+    $this->database = mysqli_connect($serverName, $userName, $password,$databaseName);      
     if (!$this->database) {
     die("Connection failed: " . mysqli_connect_error());
     exit;
     }
 
     }
-
-
    public function Initailization($getclient_id,$getclient_secret)
    {
     if (isset($getclient_id)) {
-
-
         if (mysqli_query($this->database,"INSERT INTO `user`(`client_id`,`client_secret`) VALUES ('$getclient_id','$getclient_secret');"))
         {
         $url="https://login.wrike.com/oauth2/authorize/v4?client_id=".$getclient_id."&response_type=code";
         header("Location:".$url);
         }
         else{
-
             return -1;
         }
     }
     else
     {
         return -1;
-
     }
     } 
     public function generateAccessToken($mycode)
     {
         $getclient_id="";
         $getclient_secret="";
-        $last_id="";
- 
+        $getlast_id="";
        if(!$this->database) 
        {
         return -1;
        }
-
         $sql = "SELECT * FROM user";
         $result = mysqli_query($this->database, $sql);
         $counts=mysqli_num_rows($result);
         if ($counts > 0) {
          $row = mysqli_fetch_assoc($result); 
-            $last_id=$row['id'];
+            $getlast_id=$row['id'];
             $getclient_id=$row["client_id"];
             $getclient_secret=$row['client_secret'];
             
@@ -65,35 +56,30 @@ class Wrike
         else {
             return -1;
         }
-        $this->Id=$last_id;
+        $this->Id=$getlast_id;
         $counts=0;
-        $res=mysqli_query($this->database,"SELECT * FROM `user` WHERE id='$last_id';");
+        $res=mysqli_query($this->database,"SELECT * FROM `user` WHERE id='$getlast_id';");
         $row= mysqli_fetch_assoc($res);
         $counts=mysqli_num_rows($res);
         if ($counts>0 )
-         {
+        {
 
          $getclient_id=$row['client_id'];
          $getclient_secret=$row['client_secret'];
          
          }
-         
         $url="https://login.wrike.com/oauth2/token";
         $grant_type="authorization_code";
         $client_id =$getclient_id;
         $client_secret =$getclient_secret; 
         $code = $mycode;
-        echo $client_id."    ".$client_secret."     ".$last_id;
         $dataArray= array('client_id' =>$client_id,'client_secret'=>$client_secret ,'grant_type'=>$grant_type,'code'=>$code);
-
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $dataArray);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
         $result=curl_exec($curl);
-      
         if($e=curl_error($curl))
         {
             echo $e;
@@ -107,17 +93,13 @@ class Wrike
         $tokenArray[$i]=$key;
         $i++;
         }
-
-        if (mysqli_query($this->database,"UPDATE `user` SET authorize_code ='$tokenArray[0]', refresh_authorize_token = '$tokenArray[4]' WHERE id=".$last_id.";"))
-        {
-            
+        if (mysqli_query($this->database,"UPDATE `user` SET authorize_code ='$tokenArray[0]', refresh_authorize_token = '$tokenArray[4]' WHERE id=".$getlast_id.";"))
+        {   
             return 1;
         }
         else{
-
             return -1;
         }
-
         mysqli_close($this->database);
         }
         curl_close($curl);
@@ -126,13 +108,11 @@ class Wrike
    
    public function generaterefreshAccessToken()
    {
-        
-        $last_id="";   
+        $getlast_id="";   
         $getclient_id="";
         $getclient_secret="";
         $getauthorize_token="";
         $getrefresh_authorize_token="";
-      
        if(!$this->database) 
        {
         return -1;
@@ -143,20 +123,18 @@ class Wrike
         $counts=mysqli_num_rows($result);
         if ($counts > 0) {
          $row = mysqli_fetch_assoc($result); 
-            $last_id=$row['id'];   
+            $getlast_id=$row['id'];   
             $getclient_id=$row['client_id'];
             $getclient_secret=$row['client_secret'];
             $getauthorize_token=$row['authorize_code'];
             $getrefresh_authorize_token=$row['refresh_authorize_token'];
-     
         } 
         else {
             return -1;
         }
-        
-        $this->Id=$last_id;
+        $this->Id=$getlast_id;
         $counts=0;
-        $res=mysqli_query($this->database,"SELECT * FROM `user` WHERE id='$last_id';");
+        $res=mysqli_query($this->database,"SELECT * FROM `user` WHERE id='$getlast_id';");
         $row= mysqli_fetch_assoc($res);
         $counts=mysqli_num_rows($res);
         if ($counts>0 )
@@ -166,29 +144,19 @@ class Wrike
             $getauthorize_token=$row['authorize_code'];
             $getrefresh_authorize_token=$row['refresh_authorize_token'];
          }
-    //  echo "1:  ".$getclient_id."  ------  ".$getclient_secret." -------   ".$getauthorize_token." -----------  ".$getrefresh_authorize_token;   
-
-        
         $url="https://login.wrike.com/oauth2/token";
         $grant_type="refresh_token";
         $scope="Default";
         $client_id = $getclient_id;
         $client_secret = $getclient_secret;
         $refresh_token = $getrefresh_authorize_token;    
-
-       
         $dataArray= array('client_id' =>$client_id,'client_secret'=>$client_secret ,'grant_type'=>$grant_type,'refresh_token'=>$refresh_token,'scope'=>$scope);
-
-
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $dataArray);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-
         $result=curl_exec($curl);
-      
         if($e=curl_error($curl))
         {
             echo $e;
@@ -203,7 +171,7 @@ class Wrike
         $i++;
         }
 
-        if (mysqli_query($this->database,"UPDATE `user` SET authorize_code ='$tokenArray[0]', refresh_authorize_token = '$tokenArray[4]' WHERE id=".$last_id.";"))
+        if (mysqli_query($this->database,"UPDATE `user` SET authorize_code ='$tokenArray[0]', refresh_authorize_token = '$tokenArray[4]' WHERE id=".$getlast_id.";"))
         {
             
             return 1;
@@ -217,8 +185,6 @@ class Wrike
         }
         curl_close($curl);
    } 
-
-
     public function Usingaccesstokenfindcontacts()
     {
         $res=mysqli_query($this->database,"SELECT * FROM `user` WHERE authorize_code is not NULL;");
@@ -236,12 +202,8 @@ class Wrike
          {
             return -1;
          }
-        
-
         $url="https://www.wrike.com/api/v4/contacts?me=true";
         $token=$getauthorize_token;
-        
-
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -263,12 +225,9 @@ class Wrike
         $tokenArray[$i]=$key;
         $i++;
         }
-
         }
         curl_close($curl);
         return $tokenArray;        
-
-   
     }
     public function Usingaccesstokenfindfoldersapi()
     {
@@ -287,12 +246,8 @@ class Wrike
          {
             return -1;
          }
-        
-
         $url="https://www.wrike.com/api/v4/folders";
         $token=$getauthorize_token;
-        
-
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -314,7 +269,6 @@ class Wrike
         $tokenArray[$i]=$key;
         $i++;
         }
-
         }
         curl_close($curl);
         return $tokenArray; 
